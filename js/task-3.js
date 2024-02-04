6942bc35a88537b51b04c4ab02002b96eb9c3dc9
vladostapovich <149789956+vladostapovich@users.noreply.github.com>
2024-02-04 12:56:39 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth (content, padding, border) {
+     content = Number.parseFloat(content);
+     padding = Number.parseFloat(padding);
+     border = Number.parseFloat(border);
+    return  content + padding * 2 + border * 2;
+}
+
+     getElementWidth("50px", "8px", "4px")
+     getElementWidth("60px", "12px", "8.5px")
+     getElementWidth("200px", "0px", "0px")
+         
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
